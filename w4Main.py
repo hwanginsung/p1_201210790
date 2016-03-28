5bf5cb41bafa69c5380b17131aab60b56749fed8
hwang <harr1@naver.com>
2016-03-28 13:49:33 +0900

    homework

---
+++ w4Main.py
@@ -0,0 +1,16 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def makeSwirlSquares(size,bigger,sides,angle):
+    t1.home()
+    t1.clear()
+    for i in range(0,sides):
+        if (i%2):
+            size=size+bigger
+        t1.forward(size)
+        t1.right(angle)
+        
+makeSwirlSquares(10,10,10,90)
+
+wn.exitonclick()
