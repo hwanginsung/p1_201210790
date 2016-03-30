853922e88166b8ea2dd21404d12a685a0d18a08b
hwanginsung <hinsung9@gmail.com>
2016-03-30 15:32:19 +0900

    Added files via upload

---
+++ w5Main6.py
@@ -0,0 +1,21 @@
+﻿def computerGrade(marks):
+    if(90<=marks<=100):
+        grade="A"
+    elif(80<=marks and mark<90):
+        grade="B"
+    elif(70<=marks and mark<80):
+        grade="C"
+    elif(60<=marks and mark<70):
+        grade="D"
+    elif(60<=marks):
+        grade="F"
+    return grade
+
+def lab1():
+	marks=raw_input("enter marks")
+	marks=float(marks)
+	mygrade=computerGrade(marks)
+	print mygrade
+
+def main():
+    lab1()
