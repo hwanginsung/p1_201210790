1ff81e88ff36f5af71f7330dee8eac313b64c6aa
hwanginsung <hinsung9@gmail.com>
2016-04-03 17:38:21 +0900

    homework

---
+++ w6Main6.py
@@ -0,0 +1,30 @@
+﻿def rockPaperScissor(player1,player2):
+    if player1=='rock':
+        if player2=='rock':
+            res="Same!"
+        elif player2=='paper':
+            res="player2 win!"
+        elif player2=='scissor':
+            res="player1 win!"
+    elif player1=='paper':
+        if player2=='rock':
+            res="player1 win!"
+        elif player2=='paper':
+            res="Same!"
+        elif player2=='scissor':
+            res="player2 win!"
+    elif player1=='scissor':
+        if player2=='rock':
+            res="player2 win!"
+        elif player2=='paper':
+            res="player1 win!"
+        elif player2=='scissor':
+            res="Same"
+    print "player 1 {0:s} player 2 {1:s}, result is {2:s}".format(player1,player2,res)
+    return res
+
+player1=raw_input('RockPaperScissor : ')
+player2=raw_input('RockPaperScissor : ')
+
+result=rockPaperScissor(player1,player2)
+
