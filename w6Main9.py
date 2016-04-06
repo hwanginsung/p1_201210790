ecb815bba3ded39fd178dff847b084d079dc25e0
hwanginsung <hinsung9@gmail.com>
2016-04-06 15:49:53 +0900

    homework

--- w6Main9.py
+++ w6Main9.py
@@ -5,3 +5,28 @@
             sum=sum+i
     print sum
     return sum
+
+years=raw_input("input year: ")
+year=float(years)
+
+def leapYear(year):
+    if (year%4==0) and (year%100!=0 or year%400==0):
+        res="leap year"
+    else:
+        res="not leap year"
+    return res
+
+res=leapYear(year)
+leapYear(year)
+
+def lab6():
+    sumOfMultiple3_5(0,1001)
+    leapYear()
+
+def main():
+    lab6()
+
+if __name_=="__main__":
+    main()
+
+raw_input()
