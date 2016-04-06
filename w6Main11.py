46abee930880e8a42b2714dd0a278efbecced628
hwanginsung <hinsung9@gmail.com>
2016-04-06 15:30:00 +0900

    homework

---
+++ w6Main11.py
@@ -0,0 +1,12 @@
+﻿years=raw_input("input year: ")
+year=float(years)
+
+def leapYear(year):
+    if (year%4==0) and (year%100!=0 or year%400==0):
+        res="leap year"
+    else:
+        res="not leap year"
+    return res
+
+res=leapYear(year)
+leapYear(year)
