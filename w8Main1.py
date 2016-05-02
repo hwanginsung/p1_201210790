cb1c0fed36e1baf8b4997d4466ce26bae8501eec
hwanginsung <hinsung9@gmail.com>
2016-05-02 13:15:31 +0900

    homework

---
+++ w8Main1.py
@@ -0,0 +1,19 @@
+﻿word=raw_input()
+b=dict()
+
+def charCount(word):
+    for c in word:
+        if c not in b:
+            b[c]=1
+        else:
+            b[c]=b[c]+1
+    print b
+
+
+charCount(word)
+
+def lab8():
+    charCount(word)
+    
+def main():
+    lab8()
