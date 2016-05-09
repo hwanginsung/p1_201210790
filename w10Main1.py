cb3bd22d92fd40dbf942cb4f747b4265f6c27ef3
hwanginsung <hinsung9@gmail.com>
2016-05-09 13:03:40 +0900

    homework

---
+++ w10Main1.py
@@ -0,0 +1,14 @@
+﻿coffee=[["Coffee","Water","Milk","Icecream"],
+        ["Espresso","No","No","No"],
+        ["Long Black","Yes","No","No"],
+        ["Flat White","No","Yes","No"],
+        ["Cappuccino","No","Yes","No"],
+        ["Affogato","No","No","Yes"]]
+
+coffee2=coffee[1:]
+
+for i in coffee2:
+    if i[2].upper()=="YES":
+        print len(i[2])
+
+print float(len(i[2]))/float(len(coffee2))
