e7e0ba0d491a24830ed9b3d300f461ea99b75823
hwanginsung <hinsung9@gmail.com>
2016-05-16 12:22:58 +0900

    homework

---
+++ w11Main.py
@@ -0,0 +1,24 @@
+﻿import turtle
+wn=turtle.Screen()
+wn.bgpic("C:\Users\P400\Desktop\myMaze.gif")
+t1=turtle.Turtle()
+t1.speed(1)
+t1.penup()
+t1.goto(-400,300)
+t1.pendown()
+t1.pencolor("Red")
+
+def mousegoto(x,y):
+    t1.setpos(x,y)
+
+def exit():
+    wn.bye()
+def byeBye():
+    wn.onkey(exit,"q")
+    
+def addMouse():
+    wn.onclick(mousegoto)
+
+addMouse()
+wn.listen()
+turtle.mainloop()
