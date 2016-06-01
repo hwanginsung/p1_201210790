c5396184e9e863a69c02b02de6f31eabf7be1a0c
hwanginsung <hinsung9@gmail.com>
2016-06-01 15:46:58 +0900

    Add files via upload

---
+++ w14Main(1).py
@@ -0,0 +1,23 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is",self.name
+    def talk(self):
+        print "mung mung"
+        
+class ShihTzu(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is",self.name
+    def talk(self):
+        print "si si"
+        
+class Maltese(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is",self.name
+    def talk(self):
+        print "mai mai"
